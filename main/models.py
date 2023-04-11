282d44b5281fa4b3c73bf166ad32731228a12ef0
dudtlstm <annpys1024@naver.com>
2023-04-11 16:44:22 +0900

    feat: 4주차 CR 완료

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+    # admin 페이지에서 좀 더 알아보기 쉽게 설정하는 코드
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:20]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog
+from django.utils import timezone
 
 # Create your views here.
 def mainpage(request): # 'mainpage'라는 이름 함수 선언
-    return render(request, 'main/mainpage.html')
+    blogs = Blog.objects.all() # blogs 라는 변수에 Blog의 모든 객체를 저장 = blogs에 모든 게시물 저장
+    return render(request, 'main/mainpage.html', {'blogs':blogs})
 
 def secondpage(request): # 'secondpage'라는 이름 함수 선언
     return render(request, 'main/secondpage.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.pub_date = timezone.now()
+    new_blog.body = request.POST['body']
+
+    new_blog.save()
+
+    return redirect('detail', new_blog.id)
+
+def new(request):
+    return render(request, 'main/new.html')
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, pk = id)
+    return render(request, 'main/detail.html', {'blog':blog})
